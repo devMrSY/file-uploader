31eb4912f23ab3c6cd06b3cae3142ebfa4e29ea8
devMrSY <franksooraj@gmail.com>
2022-01-19 20:48:51 +0530

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import '../App.css'
-import Home from './home'
+import '../App.css';
+import Home from './home';
 
 const App = () => {
   const [file, setfile] = useState(null);
@@ -17,28 +17,33 @@ const App = () => {
       },
     };
     const url = 'http://localhost:3001/upload';
-    axios.post(url, formData, config).then((response) => {
-      alert('Image Uploaded Successfully!!')
-    }).catch((err) => {
-      console.log('err', err)
-    })
-  }
+    axios
+      .post(url, formData, config)
+      .then((response) => {
+        alert('Image Uploaded Successfully!!');
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
+  };
 
   const onInputChange = (e) => {
-    setfile(e.target.files[0])
-  }
-  return (<>
-    <div className="App">
-      <form onSubmit={onformSubmit}>
-        <h1>Simple File Uploader</h1>
-        <div className="button">
-          <input type="file" name='photo' onChange={onInputChange} />
-          <button type="submit">Upload</button>
-        </div>
-      </form>
-    </div>
-    <Home />
-  </>)
-}
+    setfile(e.target.files[0]);
+  };
+  return (
+    <>
+      <div className="App">
+        <form onSubmit={onformSubmit}>
+          <h1>Simple File Uploader</h1>
+          <div className="button">
+            <input type="file" name="photo" onChange={onInputChange} />
+            <button type="submit">Upload</button>
+          </div>
+        </form>
+      </div>
+      <Home />
+    </>
+  );
+};
 
-export default App
+export default App;
--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import {Route, BrowserRouter, Switch} from 'react-router-dom'
-import App from '../components/App'
-import Registration from '../components/Registration'
+import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import App from '../components/App';
+import Registration from '../components/Registration';
 // import Home from '../components/home'
-import Adminlogin from '../components/admin-login'
+import Adminlogin from '../components/admin-login';
 
 export default class Routes extends React.Component {
   render() {
-    return(
+    return (
       <div>
-        <BrowserRouter> 
-            <Route  path="/" exact component={App} />
+        <BrowserRouter>
+          <Route path="/" exact component={App} />
           <Switch>
-            {/* <Route  path="/home" exact component={Home} /> */}
-            <Route  path="/admin-login" exact component={Adminlogin} />
-            <Route  path="/Registration" exact component={Registration} />
+            {/* <Route path="/home" exact component={Home} /> */}
+            <Route path="/admin-login" exact component={Adminlogin} />
+            <Route path="/Registration" exact component={Registration} />
           </Switch>
         </BrowserRouter>
       </div>
-    )
+    );
   }
 }
